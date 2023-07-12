f3c84ebef03cddeb4cabc6816ee6f4d31f026c19
gbsehnsucht <eatmeatdrinkbeer@yandex.ru>
2023-07-12 13:17:10 +0700

    push hw

---
+++ src/main/java/Box.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Box<T extends Fruit> {
+
+    private List<T> fruits = new ArrayList<>();
+
+    public void add(T fruit) {
+        fruits.add(fruit);
+    }
+
+    public int getWeight() {
+        int totalWeight = 0;
+        for (T fruit : fruits) {
+            totalWeight += fruit.getWeight();
+        }
+        return totalWeight;
+    }
+
+    public void moveTo(Box<? super T> box) {
+        box.fruits.addAll(fruits);
+        fruits.removeAll(fruits);
+    }
+}
